85dd6ed2ab9aeae6d1619ea8a174751858b4a3e4
debsouryadatta <debsouryadatta@gmail.com>
2023-05-11 13:54:45 +0530

    L1 Completed

---
+++ Summary.js
@@ -0,0 +1,22 @@
+// * #1 Introduction to Next Js, Understanding the Features and Benefits | Mastering Next Js
+// * 1.NextJS does not require to install various libraries which was needed for react, it already has those libraries installed. Its now a full stack.
+
+// * 2. In summary, server-side rendering is better for applications that require high performance, search engine optimization, and scalability. On the other hand, client-side rendering is better for applications that require a lot of interactivity and faster development time.
+
+// * 3. NextJS features - Server side rendering, Simple routing, Static site generation(Through CDN), Optimised performance(sent required code in small chunks), hot code reloading(time required for starting the server during development >>> faster), Api routes.
+
+// * 4. Advantages Over React.Js - 
+// • Search Engine Optimization
+// • Production Ready
+// • Image Optimization
+// • Data Fetching Methods
+// • 700x faster
+// • API routes
+// • Typescript Support
+
+// * 5. Next.Js 13 Features - 
+// • App Directory
+// • Layouts
+// • Server Component
+// • Fetch API Extended
+// • Turbo Pack
